f1bd6761ad6df8a6ae37f6f1df486325cc6074ae
leonard <mortuza.7@gmail.com>
2020-05-09 00:15:54 +0600

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messgaeOne.textContent = 'Loading...'
     messgaeTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + locationInput).then((response) => {
+    fetch('/weather?address=' + locationInput).then((response) => {
         response.json().then((data) => {
 
             if (data.error) {
